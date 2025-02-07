3f7b379ad71adddb0ef58f5141a50602fba60968
Muhammad Mustafa Saghir <mmustafasaghir81@gmail.com>
2025-02-07 22:46:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 
 from crewai.flow import Flow, listen, start
 
-from project4.crews.poem_crew.poem_crew import PoemCrew
+from CrewAI-Poem-Writer.crews.poem_crew.poem_crew import PoemCrew
 
 
 class PoemState(BaseModel):
